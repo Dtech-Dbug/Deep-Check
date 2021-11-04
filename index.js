7fe9ef199b9076eaad93a86be82c06bf57be88fc
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-11-04 21:25:52 +0530

    docs update : two step conditional
    - check length equality
    - if length same : check the keys intrinsic equality between two object
    - only proceed if the keys are intrinsically same

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let A = {
 };
 
 let B = {
-  day: "Firiday",
+  day: "Thursday",
   event: "Bhaifota",
 };
 
@@ -17,23 +17,40 @@ let E = {
   time: "9pm",
 };
 
-let F = {};
-let G = {};
-let C = A;
+let C = {
+  day: "friday",
+  time: "9pm",
+};
 
 function deepCheck(objOne, objTwo) {
   // * grab the keys array
   let objOneKeys = Object.keys(objOne);
   let objTwoKeys = Object.keys(objTwo);
+  let proceed;
+  let result;
 
   // * different key length means unequal contents
   if (objOneKeys.length !== objTwoKeys.length) return false;
+  // * else keys length are same
   else {
-    for (i of objOneKeys) {
-      console.log(`index : ${i} , item : ${objOneKeys[i]}`);
+    if (objOneKeys.length === objTwoKeys.length) {
+      console.log("length same");
+
+      for (i in objOneKeys) {
+        // * check if keys are same
+        if (objOneKeys[i] === objTwoKeys[i]) {
+          proceed = true;
+        } else {
+          proceed = false;
+          return proceed;
+        }
+      }
+      if (proceed) {
+        console.log("proceeding to next step check vals");
+      }
     }
   }
 }
 
-let checkResult = deepCheck(A, B);
+let checkResult = deepCheck(A, C);
 console.log(checkResult);
